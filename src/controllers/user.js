9bbf651a52d38cbd20b36d838387f2625819e004
Jody Septiawan <jody@Jodys-MacBook-Air.local>
2022-01-18 15:47:59 +0700

    update

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,6 +1,6 @@
 // Import db connection and QueryTypes from sequelize
-const db = require("../database/connection");
-const { QueryTypes } = require("sequelize");
+const db = require('../database/connection');
+const { QueryTypes } = require('sequelize');
 
 // Function addUsers for insert user data to database
 exports.addUsers = async (req, res) => {
@@ -12,19 +12,57 @@ exports.addUsers = async (req, res) => {
     await db.sequelize.query(query);
 
     res.send({
-      status: "success",
-      message: "Add user finished",
+      status: 'success',
+      message: 'Add user finished',
       query,
     });
   } catch (error) {
     console.log(error);
     res.send({
-      status: "failed",
-      message: "Server Error",
+      status: 'failed',
+      message: 'Server Error',
     });
   }
 };
 
 // Create controller get Users here ...
+exports.getUsers = async (req, res) => {
+  try {
+    const query = 'SELECT * FROM users';
+    const data = await db.sequelize.query(query, { type: QueryTypes.SELECT });
+
+    res.send({
+      status: 'success',
+      data,
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      status: 'failed',
+      message: 'Server Error',
+    });
+  }
+};
 
 // Create controller get User by received id here ...
+exports.getUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const data = await db.sequelize.query(
+      `SELECT * FROM users WHERE id = ${id}`,
+      { type: QueryTypes.SELECT }
+    );
+
+    res.send({
+      status: 'success',
+      data,
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      status: 'failed',
+      message: 'Server Error',
+    });
+  }
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 
 const router = express.Router();
 
@@ -9,18 +9,20 @@ const {
   addTodo,
   updateTodo,
   deleteTodo,
-} = require("../controllers/todo");
+} = require('../controllers/todo');
 // Get getUsers and getUser from user controller here ...
-const { addUsers } = require("../controllers/user");
+const { addUsers, getUsers, getUser } = require('../controllers/user');
 
 // Route
-router.get("/todos", getTodos);
-router.get("/todo/:id", getTodo);
-router.post("/todo", addTodo);
-router.patch("/todo/:id", updateTodo);
-router.delete("/todo/:id", deleteTodo);
+router.get('/todos', getTodos);
+router.get('/todo/:id', getTodo);
+router.post('/todo', addTodo);
+router.patch('/todo/:id', updateTodo);
+router.delete('/todo/:id', deleteTodo);
 
-router.post("/user", addUsers);
+router.post('/user', addUsers);
 // Create Route for fetching user data here ...
+router.get('/users', getUsers);
+router.get('/user/:id', getUser);
 
 module.exports = router;
